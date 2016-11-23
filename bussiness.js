11fb69c5c6577351ec7d82d9120756c2438c13c9
Meheret Egzerab <meheretgirma22@gmail.com>
2016-11-22 19:19:46 -0500

    organize

--- bussiness.js
+++ bussiness.js
@@ -2,5 +2,4 @@ var app = angular.module("myApp", ['ngRoute']);
 app.controller("myCtrl", ["$scope", function($scope){
     $scope.myPattern = 'https://images.unsplash.com/photo-1469521669194-babb45599def?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=e32c5d5b5ea606e27bf29c40b4b3bb4f'
     
-    
 }])
